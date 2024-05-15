ce369743e8290002578ee60ba5bf46ee1d27585e
albuska <albuska@users.noreply.github.com>
2024-05-15 18:53:05 +0000

    deploy: 703d1daa4822e07e69a29b141f330b8108e941b6

---
+++ assets/EventParticipants-DVZTVFQ7.js
@@ -0,0 +1,15 @@
+import{p as n,j as t}from"./index-6uaMVaf2.js";const i=n.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`,a=()=>t.jsx(i,{}),e=n.ul`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`,s=()=>t.jsx(e,{}),o=()=>t.jsxs("div",{className:"flex flex-col gap-20",children:[t.jsx(s,{}),t.jsx(a,{})]});export{o as default};
